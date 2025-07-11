d13a832475c032249f02bb5bd03fc1b204b75143
Gulyas Gergely <gulyas.gergely@hotmail.com>
2025-07-11 11:41:35 +0200

    Highlight currently selected

--- frontend/src/components/FilterToggle.vue
+++ frontend/src/components/FilterToggle.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="inline-flex rounded-md">
     <button v-for="category in categories" type="button" @click="selectOption(category.id)"
-      class="px-4 py-2 text-sm font-medium text-slate-950 bg-sky-100 rounded-xl hover:bg-sky-500 hover:text-slate-950 focus:z-10 focus:ring-2 focus:ring-sky-700 focus:text-sky-700 dark:bg-slate-800 dark:text-sky-50 dark:hover:text-sky-50 dark:hover:bg-sky-700 dark:focus:ring-sky-500 dark:focus:text-white m-1">
+      :class="[selectedCategoryId === category.id ? 'bg-sky-500' : 'bg-sky-100']"
+      class="px-4 py-2 text-sm font-medium text-slate-950 bg-sky-100 rounded-xl hover:bg-sky-300 hover:text-slate-950 focus:z-10 focus:ring-2 focus:ring-sky-700 focus:text-sky-700 dark:bg-slate-800 dark:text-sky-50 dark:hover:text-sky-50 dark:hover:bg-sky-700 dark:focus:ring-sky-500 dark:focus:text-white m-1">
       {{ category.name }}
     </button>
   </div>
